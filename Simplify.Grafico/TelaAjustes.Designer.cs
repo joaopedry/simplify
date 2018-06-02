0350cdec898f268e7c3a116da95408c78e37a48e
matheusdasilvaa <matheus.dasilva-ms@outlook.com>
2018-06-02 17:38:40 -0300

    tela ajuda

--- Simplify.Grafico/TelaAjustes.Designer.cs
+++ Simplify.Grafico/TelaAjustes.Designer.cs
@@ -76,6 +76,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(44, 49);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // TelaAjustes
             // 
